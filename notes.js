b89ea8b6435807acd69d52c2eb793745c1343a26
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-10-23 13:08:54 +0530

    Added condition for checking duplicate notes

--- notes.js
+++ notes.js
@@ -19,13 +19,21 @@ const getNotes = () => { return "Your Notes..." };
 
 const addNote = (title, body) => {
     const notes = loadNotes();
-    
-    notes.push({
-        title: title,
-        body: body,
-    })
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title
+    });
 
-    saveNotes(notes);
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body,
+        })
+    
+        saveNotes(notes);
+        console.log('New note added!');
+    } else {
+        console.log('Note title taken')
+    }
 };
 
 module.exports = {
